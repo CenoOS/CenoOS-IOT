06135686b32e31f15a11f3ce90a61ba7460eb895
nero yang <nerosoft@outlook.com>
2018-11-30 14:52:06 +0800

    sem

--- include/os_semaphore.h
+++ include/os_semaphore.h
@@ -22,19 +22,21 @@ typedef enum BINARY_SEMAPHORE_STATE{
 } BINARY_SEMAPHORE_STATE;
 
 typedef struct{
+    OSThread* suspQ;
     uint32_t count;
 } OSSemaphore;
 
 
 typedef OSSemaphore OSBinarySemaphore;
 
-void OSBinarySemaphore_create(OSBinarySemaphore* binSem,BINARY_SEMAPHORE_STATE state);
+void OSBinarySemaphore_create(OSBinarySemaphore *binSem,BINARY_SEMAPHORE_STATE state);
+void OSBinarySemaphore_delete(OSBinarySemaphore *binSem);
+void OSBinarySemaphore_acquire(OSBinarySemaphore *binSem,uint32_t timeout);
+void OSBinarySemaphore_release(OSBinarySemaphore *binSem);
 
-void OSBinarySemaphore_acquire(OSBinarySemaphore* binSem);
-
-void OSBinarySemaphore_release(OSBinarySemaphore* binSem);
-
-void OSSemaphore_create();
-
+void OSSemaphore_create(OSSemaphore *sem, uint8_t initial_count);
+void OSSemaphore_delete(OSSemaphore *sem);
+void OSSemaphore_acquire(OSSemaphore *sem,uint32_t timeout);
+void OSSemaphore_release(OSSemaphore *sem);
 
 #endif // __CENO_RTOS_SEMAPHORE_H__
--- src/os_semaphore.c
+++ src/os_semaphore.c
@@ -26,15 +26,34 @@ void OSBinarySemaphore_create(OSBinarySemaphore* binSem,BINARY_SEMAPHORE_STATE s
     __enable_irq(); /* out critial area */
 }
 
+void OSBinarySemaphore_delete(OSBinarySemaphore *binSem){
+    
+}
 
-void OSBinarySemaphore_acquire(OSBinarySemaphore* binSem){
-    while(binSem->count==NOT_AVIALIABLE){
-
-    }
+void OSBinarySemaphore_acquire(OSBinarySemaphore* binSem,uint32_t timeout){
+    while(binSem->count==NOT_AVIALIABLE){} /*愚蠢的阻塞,能不能通过降低线程优先级搞定.*/
     binSem->count = NOT_AVIALIABLE;
 }
 
 void OSBinarySemaphore_release(OSBinarySemaphore* binSem){
     binSem->count = AVIALIABLE;
+}
+
+
+
+void OSSemaphore_create(OSSemaphore *sem, uint8_t initial_count){
+
+}
+
+void OSSemaphore_delete(OSSemaphore *sem){
+
+}
+
+void OSSemaphore_acquire(OSSemaphore *sem,uint32_t timeout){
+
+}
+
+void OSSemaphore_release(OSSemaphore *sem){
+
 }
 
