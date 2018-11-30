ad1028222798d6903a4f4a102572a3a670d738f3
nero yang <nerosoft@outlook.com>
2018-11-30 13:35:17 +0800

    mq create

--- src/os_mq.c
+++ src/os_mq.c
@@ -15,7 +15,22 @@
 
 
 void OSMQ_create(OSMQ *mqPtr,uint8_t *buffer_ptr, uint32_t unit_size, uint32_t max_num_msgs){
+    if((mqPtr==NULL)||(buffer_ptr==NULL)){
+        /* bad pointer */
+    }else if((unit_size==0)||(max_num_msgs==0)){
+        /* bad value */
+    }else{
+        mqPtr->buff_ptr = buffer_ptr;
+        mqPtr->unit_size = unit_size;
+        mqPtr->max_num_msgs = max_num_msgs;
 
+        mqPtr->sendSuspQ = NULL;
+        mqPtr->receiveSuspQ = NULL;
+
+        mqPtr->insert_index = 0;
+        mqPtr->remove_index = 0;
+        mqPtr->num_msgs_stored = 0;
+    }
 }
 
 void OSMQ_delete(OSMQ *mqPtr){
@@ -31,5 +46,5 @@ void OSMQ_recive(OSMQ *mqPtr, uint32_t timeout, uint8_t *msgPtr){
 }
 
 void OSMQ_flush(OSMQ *mqPtr){
-    
+
 }
