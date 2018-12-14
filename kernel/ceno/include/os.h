3400a1d87b29559e3f0494db6d7944cc4e138559
nero yang <nerosoft@outlook.com>
2018-12-14 18:33:18 +0800

    sd

---
+++ kernel/ceno/include/os.h
@@ -0,0 +1,19 @@
+/***************************************************
+* Ceno Real-time Operating System  (CenoRTOS)
+* version 0.1
+* author neroyang
+* email nerosoft@outlook.com
+* time 2018-12-14 
+* 
+* Copyright (C) 2018 CenoCloud. All Rights Reserved 
+*
+* Contract Information：
+* https://www.cenocloud.com
+****************************************************/
+
+#ifndef __CENO_RTOS_OS_H__
+#define __CENO_RTOS_OS_H__
+
+#define OSTaskID uint32_t
+
+#endif //! __CENO_RTOS_OS_H__
